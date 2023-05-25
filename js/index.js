e43571d76338f6606b5ead32516ab28b8a48dfec
nadia.artamonova@gmail.com <nadia.artamonova@gmail.com>
2023-05-25 16:18:00 +0300

    task 2

---
+++ js/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+const modalHeading = document.querySelector('.modal__title');
+console.log(modalHeading);
+
+const modalForm = document.querySelector('.modal__form');
+console.log(modalForm);
+
+const modalDiscountCheck = document.querySelector('.modal__checkbox');
+console.log(modalDiscountCheck);
+
+const modalDiscountInput = document.querySelector('.modal__input_discount');
+console.log(modalDiscountInput);
+
